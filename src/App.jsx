d20ec281015019b07ee99fc14aa32a46411e6681
Najmiddin <your-email@example.com>
2026-06-20 08:21:00 +0300

    Bilolxon finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Section2 from "./Components/Section2/Section2";
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <h1></h1>
+      <Section2/> 
     </div>
   )
 }
--- src/Components/Section2/Section2.jsx
+++ src/Components/Section2/Section2.jsx
@@ -1,9 +1,15 @@
 import "./Section2.css";
-
+import womanImg from "../../../public/woman.png"
 function Section2() {
   return (
-    <div>
-      <h1>Bilolhon</h1>
+    <div className="father-part">
+      <img src={womanImg} alt="woman img" className="image" />
+      <div className="parts">
+        <h1 className="cost">FREE SOME COST</h1>
+        <h1 className="cost1">Save cost <br /> for you and <br /> family</h1>
+        <h5 className="text">Tellus lacus morbi sagittis lacus in. Amet nisl at <br /> mauris enim accumsan nisi, tincidunt vel. Enim <br /> ipsum, amet quis ullamcorper eget ut.</h5>
+        <h3 className="learning">Learn more➡️</h3>
+      </div>
     </div>
   )
 }
